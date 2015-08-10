01d90e97cf09be1d52b190f2781c52d03f44073f
Aji John <aji.john@gmail.com>
2015-08-10 08:47:41 -0700

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -1,13 +1,13 @@
 var ids= {
     facebook : {
-        "api_key" : "822453067806395",
-        "api_secret" : "bcf70389898fa1279c6ff26c7f6483c4",
+        "api_key" : "",
+        "api_secret" : "",
         "callback_url" : "http://localhost:3000/auth/facebook/callback"
     }
     ,
     google : {
-    	"clientID" : "410749750623-lcpgjm4uk8gik2etnq6taqeftvfet44f.apps.googleusercontent.com",
-    	"clientSecret" : "_tYTpp2cnmv0Jy9QqfI340uG",
+    	"clientID" : "",
+    	"clientSecret" : "",
     	"callbackURL" : "http://localhost:3000/auth/google/callback",
     	"realm" : "http://localhost:3000"
     }
